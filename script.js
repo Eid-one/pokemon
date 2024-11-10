e30535d387dd98590c106c6d88509c1733bdeef1
Eid Aden <e-aden@hotmail.de>
2024-11-10 17:40:43 +0100

    Popop Card is Added   but not styled yet

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let urlData = `https://pokeapi.co/api/v2/pokemon/ditto`;
 let pokemonContent = document.getElementById("pokemon");
 let searchBar = document.getElementById("searchBar");
+let pureData = []; // To store fetched Pokémon data
 
 function init() {
   fetchData();
 }
 
-let pokemonUrl = `https://pokeapi.co/api/v2/pokemon?limit=100`;
+let pokemonUrl = `https://pokeapi.co/api/v2/pokemon?limit=10`;
 
 async function fetchData() {
   let response = await fetch(pokemonUrl);
@@ -20,74 +21,82 @@ async function fetchData() {
       index + 1
     }.png`,
   }));
-  console.log(pokemon);
-
+  pureData = pokemon; // Store data for search functionality
   displayPokemon(pokemon);
 }
 
-let displayPokemon = (pokemon) => {
+function displayPokemon(pokemon) {
   let contentHTML = pokemon.map((pokeman) => HTMLRetern(pokeman)).join("");
-
   pokemonContent.innerHTML = contentHTML;
-};
-
-/* 
-function displayPokemon(allData) {
-  let contenString = allData.map((pokeman) => HTMLRetern(pokeman)).join("");
-
-  pokemonContent.innerHTML = contenString;
 }
 
-let selectPokemon = async (id) => {
-  let urlpokemon = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  let pokeman = await fetch(urlpokemon);
-  let resPokeman = await pokeman.json();
-  console.log(id);
-  displaypopop(resPokeman);
-  console.log(resPokeman);
-};
+async function selectPokemon(id) {
+  let urlSelect = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  let fetchDb = await fetch(urlSelect);
+  let responseData = await fetchDb.json();
+  displaypopop(responseData);
+}
 
-let displaypopop = (pokeman) => {
+/* function displaypopop(pokeman) {
   const type = pokeman.types.map((type) => type.type.name).join(", ");
   const image = pokeman.sprites.other["official-artwork"].front_default;
-
   const baseStatEffort = pokeman.stats[0].base_stat;
-  console.log(type);
-  let contentHTML = `
-    <div class="cardTwo">
-      <ponclick="close">  Close </p>
 
-      <br>
-      <img id="imgtwo" src="${image}" alt="${pokeman.name}">
-
-      <p>${pokeman.name} ${pokeman.id}  <u>${type}</u> </p>
-      <p>Effort: ${baseStatEffort}</p>
-       
-     
-     
-      
-    </div>
-  `;
+  let contentHTML = templateHTM(pokeman, type, baseStatEffort);
 
   pokemonContent.innerHTML = contentHTML + pokemonContent.innerHTML;
-};
+}
 
-/// Search Bar Function----
+function templateHTM(pokeman) {
+  const image = pokeman.sprites.other["official-artwork"].front_default;
+  return `
+    <div class="cardTwo">
+      <p onclick="closePopup()">Close</p>
+      <br>
+      <img id="imgtwo" src="${image}" alt="${pokeman.name}">
+      <p>${pokeman.name} ${pokeman.id}  <u>${type}</u></p>
+      <p>Effort: ${baseStatEffort}</p>
+    </div>
+  `;
+} */
 
+function displaypopop(pokeman) {
+  const type = pokeman.types.map((type) => type.type.name).join(", ");
+  const image = pokeman.sprites.other["official-artwork"].front_default;
+  const baseStatEffort = pokeman.stats[0].base_stat;
+
+  let contentHTML = templateHTM(pokeman, type, baseStatEffort);
+
+  pokemonContent.innerHTML = contentHTML + pokemonContent.innerHTML;
+}
+
+function templateHTM(pokeman, type, baseStatEffort) {
+  const image = pokeman.sprites.other["official-artwork"].front_default;
+  return `
+      <div class="cardTwo">
+        <p onclick="closePopup()">Close</p>
+        <br>
+        <img id="imgtwo" src="${image}" alt="${pokeman.name}">
+        <p>${pokeman.name} ${pokeman.id}  <u>${type}</u></p>
+        <p>Effort: ${baseStatEffort}</p>
+      </div>
+    `;
+}
+
+// Search Bar Function
 searchBar.addEventListener("keyup", (e) => {
   let searchingBar = e.target.value;
-  const filterSearch = pureData.filter((dater) => {
+  const filterSearch = pureData.filter((pokemon) => {
     return (
-      dater.name.toLowerCase().includes(searchingBar.toLowerCase()) ||
-      dater.id.toString().includes(searchingBar.toLowerCase())
+      pokemon.Name.toLowerCase().includes(searchingBar.toLowerCase()) ||
+      pokemon.Id.toString().includes(searchingBar.toLowerCase())
     );
   });
-
   displayPokemon(filterSearch);
 });
 
-init();
+function closePopup() {
+  document.querySelector(".cardTwo").remove();
+}
 
-/*  image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-        index + 1
-      }.png`, */
+init();
--- template.js
+++ template.js
@@ -1,6 +1,6 @@
 function HTMLRetern(pokeman) {
   return `
-          <div class="card"onclick="selectPokemon(${pokeman.id})" class="grass">
+          <div class="card"onclick="selectPokemon(${pokeman.Id})" class="grass">
             <div class="CardInfo">
               <h4> #${pokeman.Id}</h4>
               <h3>${pokeman.Name}</h3>
@@ -9,4 +9,17 @@ function HTMLRetern(pokeman) {
             <img id='imgCard' src="${pokeman.Image}" alt="">
           </div>
           `;
+}
+
+function templateHTM(pokeman, type, baseStatEffort) {
+  const image = pokeman.sprites.other["official-artwork"].front_default;
+  return `
+      <div class="cardTwo">
+        <p onclick="closePopup()">Close</p>
+        <br>
+        <img id="imgtwo" src="${image}" alt="${pokeman.name}">
+        <p>${pokeman.name} ${pokeman.id}  <u>${type}</u></p>
+        <p>Effort: ${baseStatEffort}</p>
+      </div>
+    `;
 }
