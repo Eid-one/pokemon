f663d730599ed5c6688a22d67313a9195f82b8fa
Eid Aden <e-aden@hotmail.de>
2024-12-21 13:42:17 +0100

    style is updated!!!

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const baseUrl = "https://pokeapi.co/api/v2/pokemon";
 let offset = 0;
-const limit = 10;
+const limit = 8;
 let pokemonContent = document.getElementById("pokomenContent");
 let searchBar = document.getElementById("searchBar");
 let missingElement = document.getElementById("noFound");
